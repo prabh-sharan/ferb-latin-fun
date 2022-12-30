202a6ed72a4674efaa620279522d6a22276efea7
cpop <prabhsharan971@gmail.com>
2022-12-31 00:18:15 +0530

    -main function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const btnTranslate= document.querySelector("#btn-translate");
 const txtInput= document.querySelector("#txt-input");
 const outputDiv= document.querySelector("#output");
@@ -8,7 +9,23 @@ function getTranslationUrl(input){
     return urlServer + "?" + "text=" + input
 }
 
-function errorHandle(error){
+function errorHandler(error){
     console.log("errorr occurred", error);
     alert("something wrong with server! try again later")
 }
+
+// click function
+function clickHandler(){
+    var inputText= txtInput.value; //take input
+
+    //call server
+    fetch(getTranslationUrl(inputText))
+      .then(response => response.json())
+      .then(json => {
+        var translatedText= json.contents.translated;
+        outputDiv.innerText=translatedText; 
+      })
+      .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
